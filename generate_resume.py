3ea74bdf64848e94a21801b86f64d403972f931f
Sean Barag <sjbarag@gmail.com>
2015-08-24 21:01:34 -0700

    Accept source file as commandline argument

--- generate_resume.py
+++ generate_resume.py
@@ -1,7 +1,8 @@
+import argparse
 import jinja2
 import os
-import yaml
 import types
+import yaml
 
 from debug.debug import dprint
 
@@ -11,31 +12,33 @@ def load_templates(template_dir=os.path.join(os.getcwd(), 'template')):
     return environment
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+            description="Generates multiple resume outputs from a singular YAML-formatted source")
+    parser.add_argument('source_file', help="yaml-formatted containing the desired resume sections")
+    args = parser.parse_args()
+
     environment = load_templates()
     dprint("found templates {}".format(environment.list_templates()))
 
-    output = open('resume.txt', 'w')
 
-    source_file = open('raw_source.yaml', 'r')
-    raw = yaml.load(source_file)
-    for section, value in zip(raw.keys(), raw.values()):
-        try:
-            # Wow this is gross.  refactor a _ton_
-            template = environment.get_template(os.path.join('plain_text',section+os.path.extsep+'txt'))
-            dprint("found template {}".format(template))
+    with open('resume.txt', 'w') as output:
+        with open(args.source_file, 'r') as source_file:
+            raw = yaml.load(source_file)
+            for section, value in zip(raw.keys(), raw.values()):
+                try:
+                    # Wow this is gross.  refactor a _ton_
+                    template = environment.get_template(os.path.join('plain_text',section+os.path.extsep+'txt'))
+                    dprint("found template {}".format(template))
 
-            
-            metatype = types.new_class(section)
-            dprint("created metatype {}".format(metatype))
-            metaobject = metatype()
-            metaobject.__dict__ = value
-            dprint("metaobject = {}".format(str(metaobject)))
-            dprint("metaobject = {}".format(metaobject.__dict__))
+                    metatype = types.new_class(section)
+                    dprint("created metatype {}".format(metatype))
+                    metaobject = metatype()
+                    metaobject.__dict__ = value
+                    dprint("metaobject = {}".format(str(metaobject)))
+                    dprint("metaobject = {}".format(metaobject.__dict__))
 
-            metaargs = dict()
-            metaargs[section] = metaobject
-            output.write(template.render(metaargs))
-        except (jinja2.TemplateNotFound):
-            print("Source section '{}' found in source file, but no template exists".format(section))
-
-    output.close()
+                    metaargs = dict()
+                    metaargs[section] = metaobject
+                    output.write(template.render(metaargs))
+                except (jinja2.TemplateNotFound):
+                    print("Source section '{}' found in source file, but no template exists".format(section))
