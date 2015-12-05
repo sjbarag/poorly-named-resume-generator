ae4f56417b028fb6f4b9d9010649628ff1c63603
Sean Barag <sjbarag@gmail.com>
2015-12-05 07:45:16 -0800

    Only move into output if python generation succeeded

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 DST="output/"
 OUTPUT_BASE="barag_resume"
 
-python3 generate_resume.py --output-name $OUTPUT_BASE --destination $DST raw_source.yaml
+python3 generate_resume.py --output-name $OUTPUT_BASE --destination $DST raw_source.yaml &&
 pushd $DST
 lualatex ${OUTPUT_BASE}.tex &&
     lualatex ${OUTPUT_BASE}.tex
