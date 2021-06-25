fd550ac5457b15c2aa99ebc0c744df5d4d8dbc07
Sean Barag <sean@barag.org>
2021-06-24 21:21:55 -0700

    filters: replace jinja2._compat with raw `rjust` call

--- pnrg/filters.py
+++ pnrg/filters.py
@@ -1,10 +1,9 @@
-from jinja2._compat import text_type
 import datetime
 import re
 
 def do_right(value, width=80):
     """Right-justifies the value in a field of a given width."""
-    return text_type(value).rjust(width)
+    return value.rjust(width)
 
 _LATEX_SUBS = (
     (re.compile(r'\\'), r'\\textbackslash'),
