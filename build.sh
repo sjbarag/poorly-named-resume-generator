169bb77fe076c8111febe68bc3baaa4912f1038b
Sean Barag <sean@barag.org>
2021-06-24 21:18:41 -0700

    build: clean before build

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@
 DST="output/"
 OUTPUT_BASE="barag_resume"
 
-python3 generate_resume.py --output-name $OUTPUT_BASE --destination $DST raw_source.yaml &&
+rm -rf $DST && mkdir $DST
+
+python3 generate_resume.py --output-name $OUTPUT_BASE --destination $DST raw_source.yaml && \
+
+cp static-content/barag_resume.cls $DST/
 pushd $DST
-lualatex ${OUTPUT_BASE}.tex &&
-    lualatex ${OUTPUT_BASE}.tex
+lualatex ${OUTPUT_BASE}.tex && lualatex ${OUTPUT_BASE}.tex
 popd
